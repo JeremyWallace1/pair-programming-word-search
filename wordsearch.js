9963d0b9c58fe87290f30478995dfe8fc3b84dea
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-23 17:13:19 -0700

    worked with Aingaran Thirunadarajah to produce code and test cases including stretch.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,48 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let output = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    output.push([]);
+  }
+  
+  for (const row in matrix) {
+    for (const col in matrix[row]) {
+      output[col][row] = matrix[row][col];
     }
-}
+  }
+  
+  return output;
+};
 
-module.exports = wordSearch
+let found = false;
+
+const wordSearch = (letters, word) => {
+
+  if (letters.length === 0) {
+    return false;
+  }
+
+  const reversedWord = word.split("").reverse().join("");
+
+  const horizontalJoin = letters.map((hls) => hls.join(""));
+
+  const vertical = transpose(letters);
+
+  const verticalJoin = vertical.map((vls)=> vls.join(""));
+  joining(verticalJoin, word);
+  joining(horizontalJoin, word);
+  joining(verticalJoin, reversedWord);
+  joining(horizontalJoin, reversedWord);
+  return found;
+};
+
+const joining = function(array, word) {
+  for (let l of array) {
+    if (l.includes(word)) {
+      found = true;
+    }
+  }
+};
+
+module.exports = wordSearch;
+
+
